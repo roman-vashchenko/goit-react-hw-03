302d3db4536beac6dcac8e8de75b5ab6b1752127
Roman Vashchenko <roman1991dp@gmail.com>
2024-05-31 01:55:05 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { nanoid } from "nanoid";
 import contactsData from "./contactsData.json";
 import { useEffect, useState } from "react";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 const savedContacts = localStorage.getItem("contacts");
 
